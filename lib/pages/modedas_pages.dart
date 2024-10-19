b326d50420d4404c503584747c04ee649df0671d
Rennan Santos <rennansantos@outlook.com>
2024-10-19 15:18:44 -0300

    1.5

--- lib/pages/modedas_pages.dart
+++ lib/pages/modedas_pages.dart
@@ -1,23 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:rotas_navegacao/models/moeda.dart';
 import 'package:rotas_navegacao/repositories/moeda_repository.dart';
+import 'package:intl/intl.dart';
 
-class MoedasPages extends StatelessWidget {
-  const MoedasPages({super.key});
+class MoedasPages extends StatefulWidget {
+ const MoedasPages({super.key});
+
+  @override
+  State<MoedasPages> createState() => _MoedasPagesState();
+}
+
+class _MoedasPagesState extends State<MoedasPages> {
+  final tabela = MoedaRepository.tabela;
+
+  NumberFormat real = NumberFormat.currency(locale: 'pt-BR', name: 'R\$');
+
+  List<Moeda> selecionada = [];
 
   @override
   Widget build(BuildContext context) {
-    final tabela = MoedaRepository.tabela;
     return Scaffold(
         appBar: AppBar(
-          title:const Text('Cripto Moedas'),
+          title: const Text('Cripto Moedas'),
           centerTitle: true,
         ),
         body: ListView.separated(
           itemBuilder: (BuildContext context, int moeda) {
             return ListTile(
-              leading: Image.asset(tabela[moeda].icone),
-              title: Text(tabela[moeda].nome),
-              trailing: Text(tabela[moeda].preco.toString()),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              leading: 
+              
+              (selecionada.contains(tabela[moeda]))?
+              const CircleAvatar(
+                child: Icon(Icons.check),
+              ):
+              
+              SizedBox(
+                width: 40,
+                child: Image.asset(
+                  tabela[moeda].icone,
+                ),
+              ),
+              title: Text(
+                tabela[moeda].nome,
+                style: const TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              trailing: Text(real.format(tabela[moeda].preco)),
+              selected: selecionada.contains(tabela[moeda]),
+              selectedTileColor: Colors.indigo[50],
+              onLongPress: () {
+               setState(() {
+                  (selecionada.contains(tabela[moeda]))?
+                selecionada.remove(tabela[moeda]):
+                selecionada.add(tabela[moeda]);
+               });
+              },
             );
           },
           padding: const EdgeInsets.all(16),
